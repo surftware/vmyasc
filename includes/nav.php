1e9932a811ba571e5073f7e721b8bfa53ec052b9
and <surftware@gmail.com>
2019-12-03 17:34:36 -0600

    avances

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top barra">
-    <a class="navbar-brand" href="#"><img src="img/logo.png" class="d-block mx-auto logo" alt="logo">
+    <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="d-block mx-auto logo" alt="logo">
         <p class="p-logo">VICTOR MONTES Y ASOCIADOS</p>
         <p class="p-logo-base">Despacho legal</p>
     </a>
@@ -53,5 +53,5 @@
         </ul>
         
     </div>
-    
+
 </nav>
