11dd2c4410a28fff756ba75353b84630c537360e
and <surftware@gmail.com>
2019-12-07 12:11:10 -0600

    optimizacionCarrusel

--- index.php
+++ index.php
@@ -12,37 +12,37 @@
   include_once ("includes/nav.php");	  
  ?>
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div id="carouselndex" class="carousel slide carousel-fade" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselndex" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselndex" data-slide-to="1"></li>
+      <li data-target="#carouselndex" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner carrusel-inner">
       <div class="carousel-item active">
         <img class="d-block w-100" src="img/carrusel/1.jpg" alt="First slide">
         <div class="transparencia1 d-block mx-auto">
-          <p class="letrasTransparencia1">Despacho jurídico</p>
+          <p class="carousel-caption">Despacho jurídico</p>
         </div>
       </div>
       <div class="carousel-item">
         <img class="d-block w-100" src="img/carrusel/2.jpg" alt="Second slide">
         <div class="transparencia1 d-block mx-auto">
-          <p>Avisos</p>
+          <p class="carousel-caption">Despacho jurídico</p>
         </div>
       </div>
       <div class="carousel-item">
         <img class="d-block w-100" src="img/carrusel/3.jpg" alt="Third slide">
         <div class="transparencia1 d-block mx-auto">
-          <p>Despacho jurídico</p>
+          <p class="carousel-caption">Despacho jurídico</p>
         </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselndex" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselndex" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
@@ -82,7 +82,7 @@
 
 
   <div class="section">
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
 
         <div class="col-md-6">
@@ -127,23 +127,23 @@
                   Servicio 1
                 </li>
                 <li>
-                Servicio 2
+                  Servicio 2
                 </li>
                 <li>
-                Servicio 3
+                  Servicio 3
                 </li>
               </ul>
             </div>
             <div class="col-md-6">
-            <ul>
+              <ul>
                 <li>
-                Servicio 4
+                  Servicio 4
                 </li>
                 <li>
-                Servicio 5
+                  Servicio 5
                 </li>
                 <li>
-                Servicio 6
+                  Servicio 6
                 </li>
               </ul>
             </div>
