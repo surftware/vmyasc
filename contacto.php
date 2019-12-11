f4354d3948e2f606b85f0063ef6b16813e363ec2
and <surftware@gmail.com>
2019-12-10 23:36:41 -0600

    avances

--- contacto.php
+++ contacto.php
@@ -42,63 +42,16 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-  <div class="block contacts" id="contacts">
-    <div class="center">
-      <div class="block__title  wow bounceInLeft">
-        <span>Get in touch</span>
-        Contact Us
-        <p>Send us a message to contact you and give you more information about the <strong>Spanish
-            School in
-            Mexico City </strong></p>
-      </div>
-      <div class="contacts__block">
-        <div class="left_side  wow bounceInLeft">
-          <ul>
-            <li><a href="mailto:vicmont@prodigy.net.mx"><i
-                  class="fa fa-envelope"></i>vicmont@prodigy.net.mx</a></li>
-            <li><a href="Tels: 5282-1930 / 5282-2030"><i class="fa fa-phone"></i>Tels: 5282-1930 / 5282-2030</a></li>
-            <li><a href="https://goo.gl/maps/A4u8GiS8XeWLhsHQ9" target="_blank"><i class="fa fa-map-marker"></i>Mexico
-                City</a></li>
-            <li><a href="skype:contact@conversationalspanishmx.com"><i
-                  class="fab fa-skype"></i></i>contact@conversationalspanishmx.com</a></li>
-          </ul>
-
-        </div>
-        <div class="right_side  wow bounceInRight">
-          <form action="index.php" method="post">
-            <div class="row">
-              <input name="nombre" type="text" class="input" placeholder="Name*" required>
-            </div>
-            <div class="row">
-              <input name="telefono" type="text" class="input" placeholder="Telephone">
-            </div>
-            <div class="row">
-              <input name="correo" type="text" class="input" placeholder="Email*" required>
-            </div>
-            <div class="row">
-              <textarea name="mensaje" placeholder="Message" class="textarea" rows="5" required></textarea>
-            </div>
-            <div class="row">
-              <div class="btn__wrap__right">
-                <input id="bSend" type="submit" class="btn btn__orange btn__large font-weight-bold" value="Send">
-              </div>
-            </div>
-          </form>
-
-        </div>
-      </div>
-
-    </div>
-  </div>
+ 
      <!-- contacts area start -->
      <div class="contacts ptb-100" id="contacts">
                      <div class="container">
                         <div class="row">
                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                               <h2 class="title tc">
-                                 Contact Us
+                                 Contaactanos
                               </h2>
-                              <p class="contacts_description tc">If you have any questions, please fill out the form below.</p>
+                              <p class="contacts_description tc">Contestaremos a la brevedad.</p>
                            </div>
                         </div>
                         <div class="row justify-content-center">
