bfe2e84605ac307e86962dfd7786cfc71aab8bd7
Cecilia GJ <cecygomjim20@comunidad.unam.mx>
2019-12-16 17:49:00 -0600

    Visitanos ,parrafo

--- contacto.php
+++ contacto.php
@@ -73,7 +73,8 @@
   <div class="text-center mb-5">
   <h1 class="h1-section text-center"> Ubicación </h1>
           <p class="text-center"> Visítanos por primera vez, toda la información que necesites será 
-          brindada personalmente .<br><br>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
+          brindada personalmente . </p>
+          <p>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.553615587871!2d-99.17735778509469!3d19.34516698693408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffe44f731a01%3A0x5eba228778ee32ab!2sInterior%204%2C%20Kappa%2011%2C%20Romero%20de%20Terreros%2C%2004310%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1576175341286!5m2!1ses-419!2smx"
       width="900" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
