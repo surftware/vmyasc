13d2f1ec8334a7550c7422839f8b42d003f85c7c
and <surftware@gmail.com>
2019-12-17 16:41:36 -0600

    acentos

--- includes/nav.php
+++ includes/nav.php
@@ -19,8 +19,8 @@
                     <span class="span-menus">La Firma</span>
                 </a>
                 <div class="dropdown-content">
-                    <p class="p-dropdown"><i class="fas fa-users" aria-hidden="true"></i>&nbsp  Qui&eacute;nes somos</p>
-                    <p class="p-dropdown"><i class="fas fa-id-card" aria-hidden="true"></i>&nbsp Testimonios</p>
+                    <p class="p-dropdown"><i class="fas fa-users" aria-hidden="true"></i>&nbsp;  Qui&eacute;nes somos</p>
+                    <p class="p-dropdown"><i class="fas fa-id-card" aria-hidden="true"></i>&nbsp; Testimonios</p>
                 </div>
             </li>
 
@@ -29,9 +29,9 @@
                     <span class="span-menus">Con&oacute;cenos</span></a>
                 <div class="dropdown-content ">
                 
-                    <p class="p-dropdown"><i class="fas fa-medal" aria-hidden="true"></i>&nbsp Logros Destacados</p>
-                    <p class="p-dropdown"><i class="fas fa-user-friends" aria-hidden="true"></i>&nbsp Nuestro Equipo</p>
-                    <p class="p-dropdown"><i class="far fa-calendar-alt" aria-hidden="true"></i>&nbsp Fechas importantes</p>
+                    <p class="p-dropdown"><i class="fas fa-medal" aria-hidden="true"></i>&nbsp; Logros Destacados</p>
+                    <p class="p-dropdown"><i class="fas fa-user-friends" aria-hidden="true"></i>&nbsp; Nuestro Equipo</p>
+                    <p class="p-dropdown"><i class="far fa-calendar-alt" aria-hidden="true"></i>&nbsp; Fechas importantes</p>
 
                 </div>
             </li>
@@ -42,9 +42,9 @@
                     <span class="span-menus">Servicios</span>
                 </a>
                 <div class="dropdown-content ">
-                    <p class="p-dropdown"><i class="fas fa-stamp"" aria-hidden="true"></i>&nbsp Derecho Penal</p>
-                    <p class="p-dropdown"><i class="fas fa-fist-raised" aria-hidden="true"></i>&nbsp Derecho Civil</p>
-                    <p class="p-dropdown"><i class="fas fa-coins" aria-hidden="true"></i>&nbsp Derecho Mercantil</p>
+                    <p class="p-dropdown"><i class="fas fa-stamp"" aria-hidden="true"></i>&nbsp; Derecho Penal</p>
+                    <p class="p-dropdown"><i class="fas fa-fist-raised" aria-hidden="true"></i>&nbsp; Derecho Civil</p>
+                    <p class="p-dropdown"><i class="fas fa-coins" aria-hidden="true"></i>&nbsp; Derecho Mercantil</p>
                 </div>
             </li>
 
@@ -53,8 +53,8 @@
                     <i class="fas fa-envelope icono"></i>
                     <span class="span-menus">Cont&aacute;cto</span>
                     <div class="dropdown-content ">
-                    <p class="p-dropdown"><i class="far fa-comment-alt" aria-hidden="true"></i>&nbsp Mensaje</p>
-                    <p class="p-dropdown"><i class="fas fa-map-marked-alt" aria-hidden="true"></i>&nbsp Ubicaci&oacute;n</p>
+                    <p class="p-dropdown"><i class="far fa-comment-alt" aria-hidden="true"></i>&nbsp; Mensaje</p>
+                    <p class="p-dropdown"><i class="fas fa-map-marked-alt" aria-hidden="true"></i>&nbsp; Ubicaci&oacute;n</p>
                 </div>
                 </a>
             </li>
