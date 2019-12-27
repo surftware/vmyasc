5229336345b907e26c0330fe1371bfa29a2f6ea8
armando arciniega <surftware@gmail.com>
2019-12-26 22:58:24 -0600

    revisión formulario de contacto

--- contacto.php
+++ contacto.php
@@ -11,25 +11,21 @@
   /*define('BASE_PATH', '/var/www/vmyasc.com/');*/
   include_once ("includes/nav.php");	  
  ?>
- <!-- Prueba Imagen Contacto-->
+  <!-- Inicia banner-->
+  <div id="carouselContacto" class="carousel slide carousel-fade" data-ride="carousel">
 
-<div id="carouselContacto" class="carousel slide carousel-fade" data-ride="carousel">
-
-<div class="carousel-inner carrusel-inner">
-  <div class="carousel-item active">
-    <img class="d-block w-100 img-fluid carrusel-inner" src="img/carrusel/vmyasc-conocenos.png" alt="First slide">
-    <div class="transparencia1 d-block mx-auto">
-      <h1 class="h1-caption">Víctor Montes y Asociados</h1>
-      <p class="p-caption">Contacto</p>
+    <div class="carousel-inner carrusel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100 img-fluid carrusel-inner" src="img/carrusel/vmyasc-conocenos.png" alt="First slide">
+        <div class="transparencia1 d-block mx-auto">
+          <h1 class="h1-caption">Víctor Montes y Asociados</h1>
+          <p class="p-caption">Contacto</p>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
+  <!-- Termina banner-->
 
-<!-- Termina banner-->
-
-
-  
 
   <!-- contacts area start -->
   <div class="contacts ptb-100" id="contacts">
@@ -46,7 +42,7 @@
     <div class="row justify-content-center">
       <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
         <div class="form">
-          <form action="../php/contact-form.php" class="form form--contacts">
+          <form action="../contacto.php" class="form form--contacts">
             <input type="text" class="form_field" placeholder="Nombre" required="">
             <input type="text" class="form_field" placeholder="Telefono" required="">
             <input type="text" class="form_field" placeholder="Email" required="">
@@ -58,21 +54,19 @@
     </div>
   </div>
   <!-- contacts area end -->
+
+  <!-- Inicia Mapa  -->
   <div class="text-center mb-5 ptb-100" id="ubicacion">
-  <h1 class="h1-section text-center"> Ubicaci&oacute;n </h1>
-          <p class="text-center"> Vis&iacute;tanos por primera vez, toda la informaci&oacute;n que necesites ser&aacute; 
-          brindada personalmente . </p>
-          <p>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
+    <h1 class="h1-section text-center"> Ubicaci&oacute;n </h1>
+    <p class="text-center"> Vis&iacute;tanos por primera vez, toda la informaci&oacute;n que necesites ser&aacute;
+      brindada personalmente . </p>
+    <p>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.553615587871!2d-99.17735778509469!3d19.34516698693408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffe44f731a01%3A0x5eba228778ee32ab!2sInterior%204%2C%20Kappa%2011%2C%20Romero%20de%20Terreros%2C%2004310%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1576175341286!5m2!1ses-419!2smx"
       width="900" height="450" frameborder="0" style="border:0;" allowfullscreen="">
     </iframe>
   </div>
-
-
-
-
-
+  <!-- Termina Mapa  -->
 
   <?php
     $remitente = $_POST['correo'];
