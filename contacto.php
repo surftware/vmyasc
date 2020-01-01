c28c866179e784ec4d886ff31277d7cd0dcb8d89
armando arciniega <surftware@gmail.com>
2020-01-01 15:29:30 -0600

    validación de formulario y alert

--- contacto.php
+++ contacto.php
@@ -28,8 +28,9 @@
 
 
   <!-- contacts area start -->
-  <div class="contacts ptb-100" id="contacts">
+  <div class="section contacts ptb-50" id="contacts">
     <div class="container">
+
       <div class="row justify-content-center">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <h1 class="h1-section text-center ">
@@ -38,46 +39,36 @@
           <p class="text-center">Contestaremos a la brevedad.</p>
         </div>
       </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
-        <div class="form">
-          <form action="../contacto.php" class="form form--contacts">
-            <input type="text" class="form_field" placeholder="Nombre" required="">
-            <input type="text" class="form_field" placeholder="Telefono" required="">
-            <input type="text" class="form_field" placeholder="Email" required="">
-            <textarea class="form_textarea" placeholder="Mensage"></textarea>
-            <button class="button button_agua button_medium mb-5" type="submit">ENVIAR</button>
-          </form>
+
+      <div class="row justify-content-center">
+        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
+          <div class="form">
+            <form action="contacto.php" class="form form--contacts" method="post">
+              <input type="text" class="form_field" placeholder="Escriba su nombre" required="" name="nombre">
+              <input type="text" class="form_field" placeholder="D&eacute;jenos su tel&eacute;fono" required="" name="telefono">
+              <input type="text" class="form_field" placeholder="Escriba su correo electr&oacute;nico" required="" name="correo">
+              <textarea class="form_textarea" placeholder="Escr&iacute;banos aqui su mensaje" name="mensaje">
+              </textarea>
+              <button class="button button_agua button_medium mb-5" type="submit">ENVIAR</button>
+            </form>
+          </div>
         </div>
       </div>
+
     </div>
   </div>
   <!-- contacts area end -->
-
-  <!-- Inicia Mapa  -->
-  <div class="text-center mb-5 ptb-100" id="ubicacion">
-    <h1 class="h1-section text-center"> Ubicaci&oacute;n </h1>
-    <p class="text-center"> Vis&iacute;tanos por primera vez, toda la informaci&oacute;n que necesites ser&aacute;
-      brindada personalmente . </p>
-    <p>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.553615587871!2d-99.17735778509469!3d19.34516698693408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffe44f731a01%3A0x5eba228778ee32ab!2sInterior%204%2C%20Kappa%2011%2C%20Romero%20de%20Terreros%2C%2004310%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1576175341286!5m2!1ses-419!2smx"
-      width="900" height="450" frameborder="0" style="border:0;" allowfullscreen="">
-    </iframe>
-  </div>
-  <!-- Termina Mapa  -->
-
   <?php
+
     $remitente = $_POST['correo'];
     $destinatario = 'and_3406@hotmail.com'; // en esta línea va el mail del destinatario.
     $asunto = 'E-mail enviado desde la página principal vmyasc.com'; // acá se puede modificar el asunto del mail
     if (!$_POST)
-    {
+    {      
 
     }
     else
-    {
+    {        
       $cuerpo =  "Nombre:   " . $_POST["nombre"]    . "\r\n"; 
       $cuerpo .= "Teléfono: " . $_POST["telefono"]  . "\r\n";
       $cuerpo .= "Email:    " . $_POST["correo"]    . "\r\n";
@@ -92,9 +83,25 @@
       $headers .= "X-Mailer: php\n";
       $headers .= "From: \"".$_POST['nombre']."\" <".$remitente.">\n";
 
-      mail($destinatario, $asunto, $cuerpo, $headers);
+      mail($destinatario, $asunto, $cuerpo, $headers);        
+      
     }
 ?>
+
+  <!-- Inicia Mapa  -->
+  <div class="text-center mb-5 ptb-50" id="ubicacion">
+    <h1 class="h1-section text-center"> Ubicaci&oacute;n </h1>
+    <p class="text-center"> Vis&iacute;tanos por primera vez, toda la informaci&oacute;n que necesites ser&aacute;
+      brindada personalmente . </p>
+    <p>Lunes a Viernes de: 10:00 am a 3:00 pm y 5:00 pm a 8:00 pm</p>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.553615587871!2d-99.17735778509469!3d19.34516698693408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ffe44f731a01%3A0x5eba228778ee32ab!2sInterior%204%2C%20Kappa%2011%2C%20Romero%20de%20Terreros%2C%2004310%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1576175341286!5m2!1ses-419!2smx"
+      width="900" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+    </iframe>
+  </div>
+  <!-- Termina Mapa  -->
+
+
 
 
   <?php
