7b5aca016bdf4a8500bd2d0d5b9dd62a5b471707
and <surftware@gmail.com>
2020-01-02 16:44:53 -0600

    avances de flex

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
           <div class="row mt-5 ">
 
 
-            <div class="col-md-6 col-sm-12 text-left mx-3">
+            <div class="col-md-6 col-sm-12 mx-3">
               <h1 class="h1-section  text-center "> Servicios</h1>
               <p>Las opciones son variadas, el objetivo es el mismo Conoce las vertientes de los servicios
                 que ofrecemos para la Prevención de Lavado de Dinero
@@ -275,13 +275,9 @@
 
       <div class="row">
 
-        <div class="col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-center bg-light centradoM">
+        <div class="col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-center bg-light">
 
-<<<<<<< HEAD
           <h1 class="h1-section text-center centroM mt-3"> Testimonios </h1>
-=======
-          <h1 class="h1-section text-center centroM mt-4"> Testimonios </h1>
->>>>>>> 066dcb54dc8b8b397b9028fe13dd79d022ed764d
           <p class="text-center"> La experiencia de nuestros miembros asesores </p>
 
 
@@ -293,14 +289,10 @@
             resolviendo todas mis dudas. Al final se ha resuelto todo favorablemente." </p>
                     
           <img src="img/abogado6.png"
-<<<<<<< HEAD
-            class="imgServicios mb-3 ml-1 border-light rounded-circle d-block mx-auto img-fluid  cel " alt="">
+            class="imgServicios mb-3 ml-1 border-light rounded-circle d-block mx-auto img-fluid " alt="">
           
 
 
-=======
-            class="imgServicios mb-3 ml-1 border-light rounded-circle d-block mx-auto img-fluid centroM" alt="">
->>>>>>> 066dcb54dc8b8b397b9028fe13dd79d022ed764d
           <a href="conocenos.php" class="d-block mx-auto">
             <h5>Raúl Pérez</h5>
             <button class="button button_agua button_medium mb-4">Leer m&aacute;s
